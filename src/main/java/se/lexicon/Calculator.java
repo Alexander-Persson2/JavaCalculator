f01a7f7597a85539975bae4bf4a1e619664d46a1
Alexander-Persson2 <alexander.persson77@gmail.com>
2024-06-10 14:39:15 +0200

    Added a while loop to the calculator

--- src/main/java/se/lexicon/Calculator.java
+++ src/main/java/se/lexicon/Calculator.java
@@ -1,39 +1,52 @@
-
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main (String[] args) {
-        int operator, n1, n2;
-        System.out.println("1 - Add \n 2 - Subtract \n 3 - Multiply \n 4 - Divide");
-        System.out.print("Choose Operator: ");
-                Scanner sc = new Scanner(System.in);
-        operator = sc.nextInt();
-        System.out.print("Enter first number : ");
-        n1 = sc.nextInt();
-        System.out.print("Enter second number : ");
-        n2 = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int operator, n1, n2, result = 0;
+        boolean continueCalculation = true;
 
-        int result = 0;
-        switch(operator) {
-            case 1:
-                result = n1 + n2;
+        while (continueCalculation) {
+            System.out.println("1 - Add \n2 - Subtract \n3 - Multiply \n4 - Divide \n5 - Exit");
+            System.out.print("Choose Operator: ");
+            operator = sc.nextInt();
+
+            if (operator == 5) {
+                continueCalculation = false;
+                System.out.println("Exiting the calculator. Welcome back anytime.");
                 break;
+            }
 
-            case 2:
-                result = n1 - n2;
-                break;
+            System.out.print("Enter first number: ");
+            n1 = sc.nextInt();
+            System.out.print("Enter second number: ");
+            n2 = sc.nextInt();
 
-            case 3:
-                result = n1 * n2;
-                break;
-
-            case 4:
-                result = n1 / n2;
-                break;
-
-            default:
-                System.out.println("Entered operator is not valid");
+            switch (operator) {
+                case 1:
+                    result = n1 + n2;
+                    break;
+                case 2:
+                    result = n1 - n2;
+                    break;
+                case 3:
+                    result = n1 * n2;
+                    break;
+                case 4:
+                    if (n2 != 0) {
+                        result = n1 / n2;
+                    } else {
+                        System.out.println("Cannot divide by zero.");
+                        continue;
+                    }
+                    break;
+                default:
+                    System.out.println("Entered operator is not valid");
+                    continue;
+            }
+            System.out.println("Result is: " + result);
         }
-        System.out.println("Result is : " + result);
+
+        sc.close();
     }
 }
